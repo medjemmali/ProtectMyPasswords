b90ddf2390e6f04b7a937784582b38879d84eb52
Mohamed Jemmali <mjemmali@neiu.edu>
2022-05-11 02:20:21 -0500

    Project

--- src/main/java/protectmypasswords/controller/MenuController.java
+++ src/main/java/protectmypasswords/controller/MenuController.java
@@ -2,64 +2,38 @@ package protectmypasswords.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
-public class MenuController  {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-
+public class MenuController implements Initializable {
     @FXML
-    private static BorderPane mainPane;
-
-//    // MenuVue1 Variables
-//    @FXML
-//    private TableView<UserData> tableview ;
-//    @FXML
-//    private TableColumn<UserData,String> addressUrl ;
-//    @FXML
-//    private TableColumn<UserData,String> username ;
-//    @FXML
-//    private TableColumn<UserData,String> password;
-@FXML
-    // MenuVue2 Variables
+    private BorderPane mainPane;
+    @FXML
     public void menuButtonOneAction(ActionEvent actionEvent) {
+
         MenuLoaderController menuLoader = new MenuLoaderController();
         Pane view = menuLoader.getPage("menuVue1");
         mainPane.setCenter(view);
-
     }
-@FXML
+    @FXML
     public void menuButtonTwoAction(ActionEvent actionEvent) {
         MenuLoaderController menuLoader = new MenuLoaderController();
         Pane view = menuLoader.getPage("menuVue2");
         mainPane.setCenter(view);
     }
-@FXML
+    @FXML
     public void menuButtonThreeAction(ActionEvent actionEvent) {
         MenuLoaderController menuLoader = new MenuLoaderController();
         Pane view = menuLoader.getPage("menuVue3");
         mainPane.setCenter(view);
     }
-//
-//    ObservableList<UserData> observableList = FXCollections.observableArrayList();
-//    @Override
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//
-//        String masterPassword = Singleton.getMasterPassword();
-//        String fileName = Singleton.getFilename();
-//
-//        UserData obj = new UserData( "firtstringOne", "String2", "String3");
-//        observableList.add(obj);
-//        addressUrl.setCellValueFactory(new PropertyValueFactory<UserData,String>("addressUrl"));
-//        username.setCellValueFactory(new PropertyValueFactory<UserData,String>("userName"));
-//        password.setCellValueFactory(new PropertyValueFactory<UserData,String>("password"));
-//        tableview.setItems( observableList);
-//    }
-//
-//    public static List<UserData> observableList() throws JsonException {
-//        String password = Singleton.getMasterPassword();
-//        String fileName = Singleton.getFilename();
-//        return new JsonModule().jsonFileToList(fileName,password );
-//
-//    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
